cbeb7a7bf7540e2beb4c7c064a1fe605bd80a1d2
Henrique <henrique@van.huisste.de>
2017-06-17 13:00:33 +0200

    standaard naar https

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://contactenbeheer.app'),
+    'url' => env('APP_URL', 'https://clientenbeheer.sinterklaasbank.nl'),
 
     /*
     |--------------------------------------------------------------------------
