ea9a9b65afbb5fa50013f88be12140cb350dbe82
Henrique <henrique@van.huisste.de>
2017-06-21 18:42:44 +0200

    emails aangepast

--- resources/views/emails/verifyEmailChange.blade.php
+++ resources/views/emails/verifyEmailChange.blade.php
@@ -288,7 +288,7 @@
                   <table border="0" cellpadding="0" cellspacing="0">
                     <tr>
                       <td>
-                        <p>Beste {{ ucfirst($voornaam) }},</p>
+                        <p>Beste {{ ucfirst($user->voornaam) }},</p>
                         <p>U heeft aangegeven dat u uw emailadres wilt wijzigen. De Sinterklaasbank verzoekt u dit emailadres te verifiëren door op de onderstaande knop te klikken. Wanneer u uw emailadres heeft geverifiëerd zal de Sinterklaasbank kunt u weer inloggen.</p>
                         <table border="0" cellpadding="0" cellspacing="0" class="btn btn-primary">
                           <tbody>
