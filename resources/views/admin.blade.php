d3a8c9192707f764963938616ebf6397153ec06a
Henrique <henrique@van.huisste.de>
2017-06-21 14:59:38 +0200

    emails downloadspagina openen/sluiten

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -69,8 +69,7 @@
                     @elseif ($settings['inschrijven_gesloten'] == 1 && $settings['downloads_ingeschakeld'] == 0) {{-- Downloads aktief --}}
                         <br><br>
                         <div class="panel panel-danger">
-                              <div class="panel-heading"><span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span><b> | De downloadpagina is niet actief!!! De inschrijvingen zijn wel gesloten! Er kan niets meer worden gewijzigd of toegevoegd door de intermediairs maar ze kunnen nog niets downloaden!.</b></div>
-                        </div>  chrijvingen aan staan! Als dit niet de bedoeling is, zet de downloads dan uit, omdat.</b></div>                                            
+                              <div class="panel-heading"><span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span><b> | De downloadpagina is niet actief!!! De inschrijvingen zijn wel gesloten! Er kan niets meer worden gewijzigd of toegevoegd door de intermediairs maar ze kunnen nog niets downloaden!.</b></div>                                            
                     @endif
 
 
