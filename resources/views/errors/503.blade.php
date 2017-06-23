b3047636c6dc90489e631a980afd5adb400ddf22
Henrique <henrique@van.huisste.de>
2017-06-23 21:07:32 +0200

    error pagina's

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Be right back.</title>
+        <title>Sorry!</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
@@ -40,7 +40,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Be right back.</div>
+                <div class="title">Momenteel zijn we bezig met onderhoud, binnenkort zijn we weer tot uw dienst!</div>
             </div>
         </div>
     </body>
