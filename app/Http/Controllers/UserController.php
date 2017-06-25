5367924952e4c837b6874155bf7555a4b04fe82b
Henrique <henrique@van.huisste.de>
2017-06-25 21:23:24 +0200

    gebruikerslijst (user/index) id DESC

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -32,7 +32,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::get();
+        $users = User::orderBy('id', 'DESC')->get();
         $loggedinuser = Auth::user();
 
         // Intermediairs mogen de index niet zien        
