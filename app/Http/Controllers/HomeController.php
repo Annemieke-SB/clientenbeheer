7a4e78a598db860765fb300d234ece21a4822a8e
Henrique <henrique@van.huisste.de>
2017-06-28 14:21:56 +0200

    tellingen bug opgelost

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -190,7 +190,7 @@ class HomeController extends Controller
         }
 
 
-        return view('tellingen', ['kids_qualified'=>$kids_qualified, 'kids_disqualified'=>$kids_disqualified, 'families_qualified'=>$families_qualified, 'families_disqualified'=>$families_qualified, 'intermediairs_totaal'=>$intermediairs_totaal, 'families_definitiefdisqualified'=>$families_definitiefdisqualified, 'families_totaal'=>$families_totaal, $kids_metbarcode=>'kids_metbarcode']);     
+        return view('tellingen', ['kids_qualified'=>$kids_qualified, 'kids_disqualified'=>$kids_disqualified, 'families_qualified'=>$families_qualified, 'families_disqualified'=>$families_qualified, 'intermediairs_totaal'=>$intermediairs_totaal, 'families_definitiefdisqualified'=>$families_definitiefdisqualified, 'families_totaal'=>$families_totaal, 'kids_metbarcode'=>$kids_metbarcode]);     
     }
 
     public function kinderlijst()
