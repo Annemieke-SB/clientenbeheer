e83592d6295e1ad3d6e27580d1bfa5baae3cac21
Henrique <henrique@van.huisste.de>
2017-07-01 11:48:44 +0200

    404 bij twee keer verificatielink aangepast

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -58,7 +58,7 @@
                 @endif
                   
                 </div>
-                {{request()->method()}}
+                {{request()->url()}}
             </div>
         </div>
     </body>
