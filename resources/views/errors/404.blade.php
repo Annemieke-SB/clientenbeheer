d8667d582f5d8641f684d99e3ca2eb3727f28984
Henrique <henrique@van.huisste.de>
2017-07-01 11:37:15 +0200

    404 bij twee keer verificatielink aangepast

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -44,7 +44,7 @@
 
                 <div class="jumbotron">
                 <div class="title">Oeps!</div>
-                @if (Request()->is('register/verify'))
+                @if (request()->is('register/verify'))
                   <h1>De verificatielink is al gebruikt!</h1>
                   <p>Klopt dit niet? Kopieer dan het adres en mail het de <a href="mailto:webmaster@sinterklaasbank.nl">webmaster</a></p>
                   <a href="{{ url('login') }}"><button type="button" class="btn btn-default navbar-btn btn-sm text-right"><span class="glyphicon glyphicon-arrow-left"></span>&nbsp;&nbsp;Log in</button></a>
