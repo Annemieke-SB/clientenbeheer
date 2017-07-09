7023720a70ec50baccb795d568b164feb19b7cbb
Henrique <henrique@van.huisste.de>
2017-07-09 10:13:27 +0200

    paginate users/index

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -37,9 +37,9 @@
                 <div class="panel-body">                    
                     <br><br>
                     <div class="row">
-                        <div class="col-sm-6 col-md-offset-5">
+                        
                             {{$users->render()}}
-                        </div>
+                        
                     </div>
                     <div class="table-responsive">
                         <table id="table" name="table" class="table table-striped table-bordered table-hover table-condensed">
