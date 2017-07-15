a9fb40b9eac6104c17b7f28734c0a19e66655d30
Henrique <henrique@van.huisste.de>
2017-07-15 10:09:15 +0200

    redirectifauthenticated clossure weggehaald

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -7,5 +7,18 @@ use Illuminate\Support\Facades\Auth;
 
 class RedirectIfAuthenticated
 {
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string|null  $guard
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
 
+
+        return;
+    }
 }
