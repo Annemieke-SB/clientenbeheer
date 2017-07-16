0a787e4523aa4c4388ef4b801a121dcff1882b5d
Henrique <henrique@van.huisste.de>
2017-07-16 13:04:16 +0200

    pagescontroller added

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use DB;
+use App\Setting;
+
+
+
+class PagesController extends Controller
+{
+
+    public function inschrijven()
+    {
+        return view('inschrijven');        
+    }  
+
+    public function particulier()
+    {
+        return view('particulier');        
+    }       
+
+    public function voorwaarden()
+    {
+        return view('voorwaarden');        
+    }   
+
+    public function home()
+    {
+        return redirect('home');    
+    }   
+
+}
