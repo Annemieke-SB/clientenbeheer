eb812dae3a1e5b36ce9c06cf3fac1d2f4e5b8ab8
Henrique <henrique@van.huisste.de>
2017-07-16 14:35:13 +0200

    paginate in intermediair-overzicht

--- resources/views/intermediairs/index.blade.php
+++ resources/views/intermediairs/index.blade.php
@@ -48,7 +48,7 @@
                             {{$intermediairs->render()}}
                         </div>
 
-                    </div>
+                    </div><small>Sortering: Laatste komt al eerste</small>
                     <table id="table" name="table" class="table table-striped table-bordered table-hover table-condensed">
                             <thead>
                             <tr>
