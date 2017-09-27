acbcf7031a25f18237eafb942b758a243f9741dc
Henrique <henrique@van.huisste.de>
2017-09-27 08:40:57 +0200

    sluitingsmededeling aan login toegevoegd

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,6 +19,10 @@
                     <div class="alert alert-primary fade in">
                       U kunt tot 10 november 2017 inschrijven. Lees <a href"">meer</a>.  
                     </div>
+
+                    <div class="alert alert-primary" role="alert">
+  This is a primary alert with <a href="#" class="alert-link">an example link</a>. Give it a click if you like.
+</div>
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
                         {{ csrf_field() }}
 
