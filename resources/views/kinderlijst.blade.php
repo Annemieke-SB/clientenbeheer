da89450569e4a884decd4fff2cbc9f76eb52ad6c
Henrique <henrique@van.huisste.de>
2017-10-23 22:56:03 +0200

    kinderlijst paginate toegevoegd

--- resources/views/kinderlijst.blade.php
+++ resources/views/kinderlijst.blade.php
@@ -57,7 +57,7 @@
         <li>
 
             <p class="navbar-text">Filter: </p>
-
+{{Route::input('wai')}}
             @if (Route::input('wai'))
                 wel <span class="badge" data-toggle="tooltip">AI<span>
             @elseif(Route::input('gai'))
