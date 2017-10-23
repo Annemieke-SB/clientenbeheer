86abf02f8b23324ef92398b427906306f7dccf67
Henrique <henrique@van.huisste.de>
2017-10-23 22:18:04 +0200

    kinderlijst paginate toegevoegd

--- resources/views/kinderlijst.blade.php
+++ resources/views/kinderlijst.blade.php
@@ -28,7 +28,7 @@
 
 <form action="{{ url('/kinderlijst') }}" method="get" class="form-inline">
   <div class="form-group">
-    <label for="exampleInputName2">Achternaam</label>
+    <label for="exampleInputName2">Achternaam bevat (mag fragment zijn)</label>
     <input type="text" class="form-control" id="exampleInputName2" name="achternaam" placeholder="Jansen">
   </div>
   <button type="submit" class="btn btn-default">Zoek</button>
