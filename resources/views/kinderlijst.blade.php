a03fe5d81ae997db7ae235ac7f4fa2c5415b1d79
Henrique <henrique@van.huisste.de>
2017-10-23 21:14:41 +0200

    kinderlijst paginate toegevoegd

--- resources/views/kinderlijst.blade.php
+++ resources/views/kinderlijst.blade.php
@@ -19,7 +19,14 @@
 
                 <div class="panel-body">
                                  <a href="{{ url('/home/') }}"><button type="button" class="btn btn-default navbar-btn btn-sm text-right"><span class="glyphicon glyphicon-arrow-left"></span>&nbsp;&nbsp;Terug</button></a>
-                <p>Hieronder staat een lijst met alle {{$kids->total()}} kinderen in de database.</p>
+                
+                                 Filters (<a href="{{ url('/users/index') }}">verwijder filter(s)</a>): <br>
+                                 <a href="{{ url('/kinderlijst') }}/?ai=1">Toon alleen AI</a> | <a href="{{ url('/kinderlijst') }}/?ai=0">Toon juist geen AI</a><br>
+                                 <a href="{{ url('/kinderlijst') }}/?a=1">Toon alleen A</a> | <a href="{{ url('/kinderlijst') }}/?a=0">Toon juist geen A</a><br>
+                                 <a href="{{ url('/kinderlijst') }}/?p=1">Toon alleen PDF</a> | <a href="{{ url('/kinderlijst') }}/?p=0">Toon juist geen PDF</a><br> <br>                                
+
+                                <p>Hieronder staat een lijst met alle {{$kids->total()}} kinderen in de database (met toepassing van het filter).</p>
+
 
                                     <div class="row">
                         <div class="col-sm-8 col-md-offset-2">
