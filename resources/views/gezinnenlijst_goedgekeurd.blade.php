f815c45049aef0cffac911e9cf65f340f0e60fa8
Henrique <henrique@van.huisste.de>
2017-10-29 14:07:23 +0100

    losse gezinnenlijstpaginas

--- resources/views/gezinnenlijst_goedgekeurd.blade.php
+++ resources/views/gezinnenlijst_goedgekeurd.blade.php
@@ -88,8 +88,8 @@
                                 <tr>
 
 
-                                <td>{{ str_limit($goedgekeurdefamilie->achternaam, 9)  }}&nbsp;</td>
-                                <td>{{ str_limit($goedgekeurdefamilie->woonplaats, 9)  }}&nbsp;</td>
+                                <td>{{ str_limit($goedgekeurdefamilie->achternaam, 12)  }}&nbsp;</td>
+                                <td>{{ str_limit($goedgekeurdefamilie->woonplaats, 12)  }}&nbsp;</td>
                                 <td>
                                         @if ($goedgekeurdefamilie->redenafkeuren)
                                             <span class="glyphicon glyphicon-envelope" data-toggle="tooltip" title="{{$goedgekeurdefamilie->redenafkeuren}}"></span>
