5401448522a29204c961dd97d5bd608209dafbe6
Henrique <henrique@van.huisste.de>
2017-10-29 14:36:00 +0100

    zoekfunctie gebruikers

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -35,7 +35,7 @@ class UserController extends Controller
         $loggedinuser = Auth::user();
 
         if (request()->has('an')) {
-            $users = User::where('achternaam', 'like', request('an') . "%")->orderBy('achternaam', 'ASC')->paginate(5)->appends('an', request('an'));
+            $users = User::where('achternaam', "%" . 'like', request('an') . "%")->orderBy('achternaam', 'ASC')->paginate(5)->appends('achternaam', request('achternaam'));
         } elseif (request()->has('na')) {
             $users = User::where('activated', '0')->orderBy('achternaam', 'ASC')->paginate(5)->appends('na', request('na'));
         } else {
