9a422e0a85827e608f648fdc09697294bf2361c9
Henrique <henrique@van.huisste.de>
2017-10-29 14:25:30 +0100

    zoekfunctie gebruikers

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -38,6 +38,45 @@
                 <div class="panel-body">            
 
                       
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <div class="navbar-brand"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></div>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        
+        <li class="active"><a href="{{ url('/gezinnenlijst_goedgekeurd') }}">Reset</a></li>
+        
+      </ul>
+      <form class="navbar-form navbar-left" action="{{ url('/gezinnenlijst_goedgekeurd') }}" method="get">
+        <div class="form-group">
+          <input type="text" class="form-control" name="achternaam"  placeholder="Achternaam gezin">
+        </div>
+        <button type="submit" class="btn btn-default">Zoek</button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li>
+
+            <p class="navbar-text">
+
+                @if (Request::input('achternaam'))
+                    <b>bevat "{{Request::input('achternaam')}}"</b>          
+                @else
+                    Geen filter
+                @endif
+
+            </p>
+        </li>
+        <li><p class="navbar-text"><p class="navbar-text"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> <b>{{$goedgekeurdefamilies->total()}}</b></p></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
                    
 
                     
