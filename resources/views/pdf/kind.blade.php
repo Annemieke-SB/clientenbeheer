4359058fd46936afd9393378715683d8b9dade19
Henrique <henrique@van.huisste.de>
2017-11-08 15:02:16 +0100

    brief

--- resources/views/pdf/kind.blade.php
+++ resources/views/pdf/kind.blade.php
@@ -58,30 +58,28 @@
 	<p>Hier een brief van Sinterklaas.</p>
 	<p>
 	Ik vind het heel fijn dat ik weer in Nederland ben. Samen met mijn Pieten ga ik weer heel veel
-	kindjes blij maken met een cadeau. En één van die kindjes ben jij! </p>
+	kindjes blij maken met een cadeau.<br>En één van die kindjes ben jij! </p>
 	<p>
-	Heb je gekeken toen ik met mijn Pieten aankwam op televisie? Wat was het spannend he?
-	Gelukkig is alles goed gegaan met de stoomboot.</p><p>
-	Omdat ik heel graag wil weten wat jij graag van Sinterklaas wilt hebben vraag ik je het verlanglijstje
-	hieronder in te vullen. Je mag plaatjes plakken...of tekenen of als je dat al kunt opschrijven wat je
-	graag wilt hebben.</p><p>
-	Heb je in het Sinterklaasjournaal gezien dat ik allemaal verschillende Pieten uit Spanje heb
-	meegenomen? Pieten in alle kleuren maar ook zwarte, witte en met roetvegen. Wat zijn ze mooi he?
-	Maar Sinterklaas is heel benieuwd wat jij nou de allermooiste, liefste en leukste Piet vindt? Zou je
-	die voor mij willen tekenen op de achterkant van je verlanglijstje? Alles mag. Als jij vindt dat hij
-	stippeltjes moet hebben of een mooie bloem op de wang dan vind ik dat heel mooi. Of misschien
-	vind je het wel leuk als Piet een roze jurk aan heeft of een brandweerpak. Maar misschien wil je wel
-	een zwarte Piet of een witte? ook die vind ik heel mooi. Ik vind gewoon alle Pieten heel erg lief en
-	mooi. Als ik alle tekeningen terug heb dan verloot ik samen met de Pieten een splinternieuwe fiets.
-	Hoe cool is dat?</p><p>
-	Als je je verlanglijstje hebt ingevuld en de allerliefste en leukste Piet hebt getekend kun je het lijstje
-	in je schoen doen. Piet komt het dan bij je ophalen. We gaan dan in de Pakjesboot kijken of het
-	cadeautje dat jij graag wilt hebben meegekomen is uit Spanje.</p><p>
-	Groetjes van Sinterklaas</p>
+	Heb je op televisie gezien hoe ik met de boot aankwam? Het is altijd zo spannend maar gelukkig is het weer
+	allemaal goed gegaan met de stoomboot.<br>
+	Samen met al mijn Pieten gaan we er weer een geweldig feest van maken.</p>
+	<p>
+	Omdat ik heel graag wil weten wat jij van Sinterklaas wilt hebben vraag ik je het verlanglijstje hieronder in te
+	vullen. Je mag plaatjes uitknippen, bijvoorbeeld uit het grote Intertoysboek en die erop plakken.
+	Maar je mag ook een tekening maken of, als je dat al kunt, opschrijven wat je graag wilt hebben.
+	</p>
+
+<p>
+	Als je je verlanglijstje hebt ingevuld mag je het in je schoen doen. Piet komt het dan bij je ophalen. We gaan dan in de pakjesboot kijken of het cadeauthe dat jij graag wilt hebben is meegekomen uit Spanje. Spannend he!</p>	
+<p>
+	Groetjes van Sint</p>
+
+	<p>Plak hier de kadootjes die je graag wilt hebben. Plak bij nr. 1 het kadootje dat je het allerliefste wilt hebben
+en bij nr. 2 wat je daarna het liefste wilt hebben. De Sint zoekt dan een mooi kadootje voor je uit.</p>
 </div>
 
 
-<div id="verlanglijstholder">{{ Html::image('img/sintbankpics/vakjes.gif', '-', array('class' => 'verlanglijst')) }}&nbsp;</div>
+<div id="verlanglijstholder">{{ Html::image('img/sintbankpics/hokjes_brief2.png', '-', array('class' => 'verlanglijst')) }}&nbsp;</div>
 
 <div class="barcode">{!! ucfirst($kid->htmlbarcode) !!}</div>
 </body>
