0a27794d094be01709f725c632d6958a45b2d90d
Henrique <henrique@van.huisste.de>
2017-11-19 17:57:47 +0100

    overige barcodes toevoegen

--- resources/views/extrabarcodes.blade.php
+++ resources/views/extrabarcodes.blade.php
@@ -52,7 +52,7 @@
                                                             
                                                             <td>
 
-                                                            <a href="{{ url('/download/extrapdf/'.$extrabarcodes->id) }}"><button type="button" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-download-alt"></span>&nbsp;PDF</button></a>                                     
+                                                            <a href="{{ url('/download/extrapdf/'.$extrabarcode->id) }}"><button type="button" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-download-alt"></span>&nbsp;PDF</button></a>                                     
                                         
                                                             @if($extrabarcode->downloadedbarcodepdf)
                                                                 <span class="glyphicon glyphicon-ok-sign" aria-hidden="true"></span>                                     
