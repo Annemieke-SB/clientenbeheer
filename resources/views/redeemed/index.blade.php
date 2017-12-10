4e46aed1f34794a4c83cab9a75e5325809f5a84b
Henrique <henrique@van.huisste.de>
2017-12-10 15:25:38 +0100

    redeemed toegevoegd

--- resources/views/redeemed/index.blade.php
+++ resources/views/redeemed/index.blade.php
@@ -19,7 +19,7 @@
 @foreach ($redeemedcodes as $redeemedcode)
 
 
-<tr><td>{{$redeemedcode->CardNumber}}</td><td>{{$redeemedcode->barcode->kid_id}}</td></tr>
+<tr><td>{{$redeemedcode->CardNumber}}</td><td>{{$redeemedcode->CardNumber}}</td></tr>
 
 @endforeach
 
