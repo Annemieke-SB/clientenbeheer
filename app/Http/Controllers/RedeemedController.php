49cc1b8e839622723af1704df4160ae97711044f
Henrique <henrique@van.huisste.de>
2017-12-10 13:53:30 +0100

    redeemed toegevoegd

--- app/Http/Controllers/RedeemedController.php
+++ app/Http/Controllers/RedeemedController.php
@@ -37,7 +37,7 @@ class RedeemedController extends Controller
             return redirect('intermediairs/show/'.$juisteintermediair->id)->with('message', 'U heeft een onjuiste pagina bezocht en bent weer teruggeleid naar uw startpagina.');
         }
 
-        $redeemedcodes = Redeemed::all();
+        $redeemedcodes = Redeemed::with('barcode');
 
         return view('redeemed.index', ['redeemedcodes' => $redeemedcodes]);
     }
