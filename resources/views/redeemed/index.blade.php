4c79c71b56633d46c99cb5a2bbb249227383d741
Henrique <henrique@van.huisste.de>
2017-12-10 16:31:45 +0100

    redeemed toegevoegd

--- resources/views/redeemed/index.blade.php
+++ resources/views/redeemed/index.blade.php
@@ -30,14 +30,21 @@
 
 
 @if($redeemedcode->CardNumber>0)
-
-	<tr><td>{{$redeemedcode->CardNumber}}</td><td>
-		@if($redeemedcode->barcode)
-			{{$redeemedcode->barcode->kid_id}}
-		@endif
-	</td>
-
-	<td>{{$redeemedcode->ValueOfRedemptions}}</td></tr>
+	@if($redeemedcode->barcode->kid_id)
+		<tr>
+			<td>
+				{{$redeemedcode->CardNumber}}
+			</td>
+			<td>
+				@if($redeemedcode->barcode)
+					{{$redeemedcode->barcode->kid_id}}
+				@endif
+			</td>
+			<td>
+				{{$redeemedcode->ValueOfRedemptions}}
+			</td>
+		</tr>
+	@endif
 
 @endif
 
