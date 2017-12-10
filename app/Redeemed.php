b7ec8d41a858b186122fe4d3aef7c48752269909
Henrique <henrique@van.huisste.de>
2017-12-10 15:24:35 +0100

    redeemed toegevoegd

--- app/Redeemed.php
+++ app/Redeemed.php
@@ -20,7 +20,7 @@ class Redeemed extends Model
 
     public function barcode()
     {
-        return $this->hasOne('App\Barcode', 'barcode', 'CardNumber');
+        return $this->hasOne('App\Barcode', 'barcode');
     }
 
 
