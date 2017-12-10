6cc28fa3135e9dc504ab1430f77a2c21f2df3afa
Henrique <henrique@van.huisste.de>
2017-12-10 16:09:42 +0100

    redeemed toegevoegd

--- resources/views/redeemed/index.blade.php
+++ resources/views/redeemed/index.blade.php
@@ -2,6 +2,13 @@
 
 @section('content')
 <div class="container">
+
+	<div class="row">
+        <div class="col-md-8 col-md-offset-2">
+
+
+
+                <div class="panel-body">
 <table>
 <thead><tr><td>Barcode</td><td>Kind</td><td>Uitgegeven</td></thead>
 
