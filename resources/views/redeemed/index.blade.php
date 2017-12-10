0d69fa20c4004a72a8b5c2116429ed0873d30683
Henrique <henrique@van.huisste.de>
2017-12-10 15:40:33 +0100

    redeemed toegevoegd

--- resources/views/redeemed/index.blade.php
+++ resources/views/redeemed/index.blade.php
@@ -7,7 +7,7 @@
 
 @if($redeemedcode->CardNumber>0)
 
-	<tr><td>{{$redeemedcode->CardNumber}}</td><td>{{$redeemedcode->barcode->kid_id}}</td></tr>
+	<tr><td>{{$redeemedcode->CardNumber}}</td><td>{{$redeemedcode->Barcode->kid_id}}</td></tr>
 
 @endif
 
