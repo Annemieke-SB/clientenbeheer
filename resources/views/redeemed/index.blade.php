4eb4e15d96fa5cbe2c8e7987981386dc6223f146
Henrique <henrique@van.huisste.de>
2017-12-10 14:59:16 +0100

    redeemed toegevoegd

--- resources/views/redeemed/index.blade.php
+++ resources/views/redeemed/index.blade.php
@@ -15,7 +15,7 @@
                             <tbody>
 
 @foreach ($redeemedcodes as $redeemedcode)
-<tr><td>{{dd($redeemedcode)}}</td><td>{{$redeemedcode->barcode->id}}</td></tr>
+<tr><td>{{dd($redeemedcode)}}</td><td>{{$redeemedcode->Barcode->id}}</td></tr>
 
 @endforeach
 
