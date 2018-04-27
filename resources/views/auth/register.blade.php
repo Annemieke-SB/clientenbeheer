afdd10c30ca7ab45c73f1a70bea82c2414e20101
hennio <hnrqvnhsstd@gmail.com>
2018-04-27 23:21:45 +0200

    register aanpassen

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -169,7 +169,7 @@
 
                             <div class="col-md-6">
                                 <div class="input-group">
-                                <span class="input-group-addon" id="basic-addon1" style="font-family: sans-serif; font-size:15px;"><span class="glyphicon glyphicon-earphone"></span></span>
+                                <span class="input-group-addon" id="basic-addon1" style="font-family: sans-serif; font-size:15px;"></span></span>
                             {!! Form::select('type', Custommade::typenIntermediairs(), null,  ["class"=>"form-control", 'autofocus']) !!}
                                 </div>
 
