8627b4f9e602f00b0e3dd789931ced2023e0ea56
hennio <hnrqvnhsstd@gmail.com>
2018-04-28 15:20:55 +0200

    bugfix

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -25,7 +25,7 @@
 <ul class="nav nav-tabs nav-justified">
   <li role="presentation" class="active"><a href="#">Home</a></li>
   <li role="presentation"><a href="{{url('/user/edit/')."/$user->id" }}">Wijzig uw gegevens</a></li>
-  <li role="presentation"><a href="#">Gezinnen toevoegen</a></li>
+  <li role="presentation"><a href="{{url('familie/toevoegen')."/$user->id"}}">Gezinnen toevoegen</a></li>
   <li role="presentation" class="disabled" data-trigger="hover" data-placement="bottom" aria-hidden="true" data-toggle="popover" title="Downloads gesloten" data-content="Downloads worden later geopend, u krijgt daarover een bericht van ons."><a href="#">Downloads</a></li>
 </ul>
 
