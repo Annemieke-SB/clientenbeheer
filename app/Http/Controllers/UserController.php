8a608c439eb5fd879a5085df2dea258b59c2b6f6
hennio <hnrqvnhsstd@gmail.com>
2018-04-28 15:10:47 +0200

    bugfix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -206,7 +206,7 @@ class UserController extends Controller
         $loggedinuser = Auth::user();
 
         // Intermediairs mogen geen andere kinderen zien dan diegene die ze zelf beheren        
-        if(($loggedinuser->usertype == 3)&&($loggedinuser->id != $user->id)){
+        if(($loggedinuser->usertype == 3)&&($loggedinuser->id != $id)){
             
             Log::info('Een intermediair probeerde een andere gebruiker te wijzigen (user.edit) te laden, userid: '.$loggedinuser->id);
             return redirect('home')->with('message', 'U heeft een onjuiste pagina bezocht en bent weer teruggeleid naar uw startpagina.');
