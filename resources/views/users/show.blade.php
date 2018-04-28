33951caf0b523538d38ab65089b4b2f202d80a49
hennio <hnrqvnhsstd@gmail.com>
2018-04-28 15:02:58 +0200

    bugfix

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -78,7 +78,6 @@
         <div class="card">
           <div class="card-body">
             <h5 class="card-title">Reden aanmelden</h5>
-							bla: {{ Auth::user()->usertype }}
             <p class="card-text">{{ $user->reden }}</p>
                                     Gebruiker geactiveerd: @if ($user->activated)
 					<span class="glyphicon glyphicon-ok-sign" aria-hidden="true" style="color:green;"></span>
