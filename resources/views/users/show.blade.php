a1a0e55ba647efb035c6ccb7fe3ed63d93e08a63
hennio <hnrqvnhsstd@gmail.com>
2018-04-28 14:59:23 +0200

    bugfix

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -85,7 +85,7 @@
                                     @else
                                         <span class="glyphicon glyphicon-remove-sign" aria-hidden="true" style="color:red;" data-toggle="tooltip" title="{{$user->reden}}"></span>&nbsp;
                                         @if ($user->emailverified)
-						@if (Auth::user()->usertype==1)
+						@if ( Auth::user()->usertype == 1)
                                             		<a href="{{ url('/user') }}/toggleactive/{{ $user->id }}" class="btn btn-primary">Wijzig</a>
                                         	@endif
                                         @endif
