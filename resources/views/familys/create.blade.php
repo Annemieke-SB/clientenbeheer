3809d87ff4a0c0ae44382bfd3568304b43635040
hennio <hnrqvnhsstd@gmail.com>
2018-04-29 15:20:12 +0200

    intermediairnav in place

--- resources/views/familys/create.blade.php
+++ resources/views/familys/create.blade.php
@@ -21,13 +21,7 @@
                 @endif
 
                 <div class="panel-body">
- <ul class="nav nav-tabs nav-justified">
-  <li role="presentation"><a href="#">Home</a></li>
-  <li role="presentation"><a href="{{ url('/user/edit/')."/$user->id" }}">Wijzig uw gegevens</a></li>
-  <li role="presentation" class="active"><a href="{{url('familie/toevoegen')."/$user->id"}}">Gezinnen toevoegen</a></li>
-  <li role="presentation" class="disabled" data-trigger="hover" data-placement="bottom" aria-hidden="true" data-toggle="popover" title="Downloads gesloten" data-content="Downloads worden later geopend, u krijgt daarover een bericht van ons."><a href="#">Downloads</a></li>
-</ul>
-
+@include ('layouts.intermediairnav',['page'=>'home'])
 
                     @if ($errors->any())
                         <ul class="alert alert-danger">
