e8aefa217fa25bc501967a6599f3375b85e33dc0
hennio <hnrqvnhsstd@gmail.com>
2018-04-29 15:11:36 +0200

    intermediairnav in place

--- resources/views/layouts/intermediairnav.blade.php
+++ resources/views/layouts/intermediairnav.blade.php
@@ -1,21 +1,26 @@
 
 <div>
 <ul class="nav nav-tabs nav-justified">
-  <li role="presentation" 
-	@if ($page = "home")
-		class="active"
-	@endif
-	><a href="#">Home</a></li>
-  <li role="presentation" 
-	@if ($page = "edit")
-		class="active"
-	@endif
-	><a href="{{url('/user/edit/')."/$user->id" }}">Wijzig uw gegevens</a></li>
-  <li role="presentation" 
-	@if ($page = "add")
-		class="active"
-	@endif
-	><a href="{{url('familie/toevoegen')}}/{{$user->id}}">Gezinnen toevoegen</a></li>
+
+@if ($page == "home")
+  <li role="presentation" class="active"><a href="#">Home</a></li>
+@else
+  <li role="presentation"><a href="#">Home</a></li>
+
+@endif
+
+@if ($page == "edit")
+  <li role="presentation" class="active"><a href="{{url('/user/edit/')."/$user->id" }}">Wijzig uw gegevens</a></li>
+@else
+  <li role="presentation"><a href="{{url('/user/edit/')."/$user->id" }}">Wijzig uw gegevens</a></li>
+@endif
+
+@if ($page == "add")
+  <li role="presentation" class="active"><a href="{{url('familie/toevoegen')}}/{{$user->id}}">Gezinnen toevoegen</a></li>
+@else
+  <li role="presentation"><a href="{{url('familie/toevoegen')}}/{{$user->id}}">Gezinnen toevoegen</a></li>
+@endif
+
   <li role="presentation" class="disabled" data-trigger="hover" data-placement="bottom" aria-hidden="true" data-toggle="popover" title="Downloads gesloten" data-content="Downloads worden later geopend, u krijgt daarover een bericht van ons."><a href="#">Downloads</a></li>
 </ul>
 
