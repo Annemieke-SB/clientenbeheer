d44a47e7c4f381ebc009fb310ae112b680a0de6a
hennio <hnrqvnhsstd@gmail.com>
2018-05-09 11:51:37 +0200

    bugfix

--- app/User.php
+++ app/User.php
@@ -43,7 +43,7 @@ class User extends Authenticatable
 
     public function getBlacklistedAttribute() {
 	
-	    return blacklist::check($this->email);
+	    return Blacklist::check($this->email);
     }
 
 
