5ccfb09f65a6a3d79a23dda21c07b7a7f760c276
hennio <hnrqvnhsstd@gmail.com>
2018-05-23 12:50:27 +0200

    edit kid in family show

--- resources/views/familys/show.blade.php
+++ resources/views/familys/show.blade.php
@@ -243,7 +243,7 @@
                                 </td>
                                 <td>
                                      <a href="{{ url('/kids') }}/show/{{ $kid->id }}"><button class="btn btn-info btn-xs" type="button"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>&nbsp;Toon</button></a>
-
+                                     <a href="{{ url('/kids') }}/edit/{{ $kid->id }}"><button class="btn btn-info btn-xs" type="button"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span>&nbsp;Wijzig</button></a>
                                         @if (Auth::user()->usertype==3)
                                             @if(!$family->aangemeld==1)
                                             <a href="#" data-toggle="modal" data-target="#deleteModal{{$kid->id}}"><button class="btn btn-danger btn-xs" type="button"><span class="glyphicon glyphicon-remove-sign" aria-hidden="true"></span>&nbsp;Wis</button></a>
