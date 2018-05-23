a117e9114b967a435bdd63c4b624f434dbcf2669
hennio <hnrqvnhsstd@gmail.com>
2018-05-23 13:05:33 +0200

    bugfix

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -175,7 +175,8 @@
                                                 <a href="{{ url('/family') }}/show/{{$family->id}}"><button class="btn btn-info btn-xs" type="button"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>&nbsp;Toon</button></a>
 
                                           
-                                                
+                                                @if (App\Setting::get('inschrijven_gesloten') == 0)                                     
+                                                    <a href="{{ url('/kids') }}/edit/{{ $kid->id }}"><button class="btn btn-info btn-xs" type="button"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span>&nbsp;Wijzig</button></a>
                                                         
                                                         <a href="#" data-toggle="modal" data-target="#deleteModal{{ $family->id }}"><button class="btn btn-danger btn-xs" type="button"><span class="glyphicon glyphicon-remove-sign" aria-hidden="true"></span>&nbsp;Wis</button></a>
                                                
@@ -198,6 +199,7 @@
                                                             </div>
                                                           </div>
                                                         </div>
+                                                @endif
 
  
                                                 
