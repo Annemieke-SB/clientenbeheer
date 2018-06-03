fd7bfa3dd727ec81a67e207c4fd641dd0acd689c
hennio <hnrqvnhsstd@gmail.com>
2018-06-03 11:01:09 +0200

    bugfix

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -34,6 +34,7 @@
                         <br>
                         <div class="alert alert-info fade in">{{ Session::get('message')}}<a href="#" class="close" data-dismiss="alert">&times;</a></div>
                         @endif
+                        <br>
                         <div class="alert alert-info fade in">test<a href="#" class="close" data-dismiss="alert">&times;</a></div>
 
 
