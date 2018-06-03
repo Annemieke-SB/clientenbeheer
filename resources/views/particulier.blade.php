1ad693e001cd52b762bb0a156faae400cc721467
hennio <hnrqvnhsstd@gmail.com>
2018-06-03 10:46:57 +0200

    bugfix

--- resources/views/particulier.blade.php
+++ resources/views/particulier.blade.php
@@ -11,7 +11,7 @@
             @endif
                         
             <div class="panel panel-default">
-                <div class="panel-heading">Intermediair dashboard</div>
+                
 
                 <div class="panel-body">
                     <h1>U bent particulier</h1>
