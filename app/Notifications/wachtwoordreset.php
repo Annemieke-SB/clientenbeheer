a384ab9a24b5ed7f3f5c7a7f4d8387645d4949ee
hennio <hnrqvnhsstd@gmail.com>
2018-06-06 11:25:01 +0200

    route voor resetmail vervangen door usercontroller

--- app/Notifications/wachtwoordreset.php
+++ app/Notifications/wachtwoordreset.php
@@ -1,31 +1,35 @@
 <?php
 
-namespace App\Notifications;
+namespace Illuminate\Auth\Notifications;
 
-use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Notification;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class wachtwoordreset extends Notification
+class ResetPassword extends Notification
 {
-    use Queueable;
+    /**
+     * The password reset token.
+     *
+     * @var string
+     */
+    public $token;
 
     /**
-     * Create a new notification instance.
+     * Create a notification instance.
      *
+     * @param  string  $token
      * @return void
      */
-    public function __construct()
+    public function __construct($token)
     {
-        //
+        $this->token = $token;
     }
 
     /**
-     * Get the notification's delivery channels.
+     * Get the notification's channels.
      *
      * @param  mixed  $notifiable
-     * @return array
+     * @return array|string
      */
     public function via($notifiable)
     {
@@ -33,7 +37,7 @@ class wachtwoordreset extends Notification
     }
 
     /**
-     * Get the mail representation of the notification.
+     * Build the mail representation of the notification.
      *
      * @param  mixed  $notifiable
      * @return \Illuminate\Notifications\Messages\MailMessage
@@ -41,21 +45,8 @@ class wachtwoordreset extends Notification
     public function toMail($notifiable)
     {
         return (new MailMessage)
-                    ->line('Hier de notificatie')
-                    ->action('De aktie', url('/'))
-                    ->line('Bedankt!');
-    }
-
-    /**
-     * Get the array representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return array
-     */
-    public function toArray($notifiable)
-    {
-        return [
-            //
-        ];
+            ->line('U krijgt deze mail omdat we een aanvraag hebben gehad om uw wachtwoord te resetten.')
+            ->action('Wijzig wachtwoord', url(config('app.url').route('password.reset', $this->token, false)))
+            ->line('Als u dit niet gedaan heeft kunt u deze mail negeren.');
     }
 }
