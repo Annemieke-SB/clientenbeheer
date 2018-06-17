bfea8b78ee11a53c3d1c24c2ffbf69d477e72e80
Henrique <henrique@van.huisste.de>
2018-06-17 22:13:15 +0200

    sortering gebruikerspagina aanpassen

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -50,12 +50,9 @@ class UserController extends Controller
 
         if (request()->input('sort')=='ad') { 
             // achternaam ascending
-            $sorts = "'achternaam', 'DESC'";
-
+            $sorts = ['achternaam', 'DESC'];
         } else { 
-            // achternaam descending
-            $sorts = "'achternaam', 'ASC'";
-
+            $sorts = ['achternaam', 'ASC'];
         } 
 
         if (request()->has('na')) { 
