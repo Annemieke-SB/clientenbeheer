a78c7e4b16aa3f89149c48afbcec18d8a54c4308
hennio <hnrqvnhsstd@gmail.com>
2018-06-24 10:35:51 +0200

    intermediair nav aangepast

--- resources/views/layouts/intermediairnav.blade.php
+++ resources/views/layouts/intermediairnav.blade.php
@@ -8,25 +8,25 @@
     @endif
 
     @if ($page == "edit")
-    <li role="presentation" class="active"><a href="{{url('/user/edit/')."/$user->id" }}">Wijzig uw gegevens</a></li>
+    <li role="presentation" class="active"><a href="{{url('/user/edit/')."/$user->id" }}">Wijzig gegevens</a></li>
     @else
         @if (Auth::user()->id != Request::segment(3))
-        <li role="presentation" class="disabled"><a href="#">Wijzig uw gegevens</a></li>
+        <li role="presentation" class="disabled"><a href="#">Wijzig gegevens</a></li>
         @else
-        <li role="presentation"><a href="{{url('/user/edit/')."/$user->id" }}">Wijzig uw gegevens</a></li>
+        <li role="presentation"><a href="{{url('/user/edit/')."/$user->id" }}">Wijzig gegevens</a></li>
         @endif
     @endif
 
     @if ($page == "add")
-    <li role="presentation" class="active"><a href="{{url('familie/toevoegen')}}{{$user->id}}">Gezinnen toevoegen</a></li>
+    <li role="presentation" class="active"><a href="{{url('familie/toevoegen')}}{{$user->id}}">Gezin toevoegen</a></li>
     @else
         @if (App\Setting::get('inschrijven_gesloten')==1)
-        <li role="presentation" class="disabled"><a href="#">Gezinnen toevoegen</a></li>
+        <li role="presentation" class="disabled"><a href="#">Gezin toevoegen</a></li>
         @else
             @if (Auth::user()->id != Request::segment(3))
             <li role="presentation" class="disabled"><a href="#">Gezin toevoegen</a></li>
             @else
-            <li role="presentation"><a href="{{url('familie/toevoegen')}}/{{$user->id}}">Gezinnen toevoegen</a></li>
+            <li role="presentation"><a href="{{url('familie/toevoegen')}}/{{$user->id}}">Gezin toevoegen</a></li>
             @endif
         @endif
     @endif
