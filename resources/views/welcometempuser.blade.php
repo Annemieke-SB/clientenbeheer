7b38195c7bc39ae9ae37e0e391c2e1ab36421a61
Henrique <henrique@van.huisste.de>
2018-07-02 21:33:54 +0200

    geactiveerde gebruikers mogen geen gezinnen aanmelden

--- resources/views/welcometempuser.blade.php
+++ resources/views/welcometempuser.blade.php
@@ -9,11 +9,11 @@
                         
             <div class="panel panel-default">
                 <div class="panel-heading">Inschrijving bijna voltooid</div>
-            <!-- Flashmessage -->
+
+                <div class="panel-body">            <!-- Flashmessage -->
             @if (count(Session::get('message')) > 0)
             <div class="alert alert-info fade in">{{ Session::get('message')}}<a href="#" class="close" data-dismiss="alert">&times;</a></div>
             @endif
-                <div class="panel-body">
                     <h1>Een ogenblikje nog..</h1>
                     <p>Bedankt voor het aanmelden! Uw inschrijving is bijna voltooid. De Sinterklaasbank moet uw aanmelding nog goedkeuren. Hier kan wat tijd overheen gaan omdat we alle aanmeldingen handmatig controleren. We hopen op uw begrip hiervoor...</p><p>U krijgt van ons een email als uw account is goedgekeurd! </p><p>Vindt u het te lang duren, neem dan contact met ons op via <a href="mailto:info@sinterklaasbank.nl">info@sinterklaasbank.nl</a>.</p><p>Met vriendelijke groet, <br><br></p><p>Stichting de Sinterklaasbank</p>
                 </div>
