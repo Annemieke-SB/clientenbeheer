b62078f9736341b99b400b732bdb6088477976eb
hennio <hnrqvnhsstd@gmail.com>
2018-09-23 17:22:46 +0200

    maillijsten toegevoegd

--- app/Http/Controllers/MaillijstenController.php
+++ app/Http/Controllers/MaillijstenController.php
@@ -41,7 +41,7 @@ class MaillijstenController extends Controller
 		      return redirect('home');
 		}
     	
-            
+        $lijst = false;       
 
             if (request()->input('lijst')=='zkg') { 
             // Zonder kinderen of gezinnen 
