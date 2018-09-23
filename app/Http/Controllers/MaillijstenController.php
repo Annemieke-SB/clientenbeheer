43bde925ba9fd5116b90a42efaa24b1d8491de4d
hennio <hnrqvnhsstd@gmail.com>
2018-09-23 17:27:55 +0200

    maillijsten toegevoegd

--- app/Http/Controllers/MaillijstenController.php
+++ app/Http/Controllers/MaillijstenController.php
@@ -46,7 +46,7 @@ class MaillijstenController extends Controller
             if (request()->input('lijst')=='zkg') { 
             // Zonder kinderen of gezinnen 
 
-                $intermediairzonderfamilies = User::where('usertype',3)->whereDoesntHave('familys')->select('email');
+                $intermediairzonderfamilies = User::where('usertype',3)->whereDoesntHave('familys')->select('email')->get();
                 $intermediairzonderkids = User::where('usertype',3)->whereDoesntHave('kids')->whereHas('familys')->select('email')->get();
 
                 $lijst = $intermediairzonderfamilies;
