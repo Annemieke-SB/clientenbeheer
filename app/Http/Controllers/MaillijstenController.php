17dbbc7d7355760b4e9b53de8e8a70495a50d464
hennio <hnrqvnhsstd@gmail.com>
2018-09-23 17:45:49 +0200

    maillijsten toegevoegd

--- app/Http/Controllers/MaillijstenController.php
+++ app/Http/Controllers/MaillijstenController.php
@@ -62,7 +62,7 @@ class MaillijstenController extends Controller
 
                 $samengevoegd = $intermediairzonderkids->merge($intermediairzonderfamilies);
 
-                $lijst = $samengevoegd;
+                $lijst = $samengevoegd->unique();
 
             }
         
