791f46de36f27928f1a1f8b19780ca6badd1ba3f
Henrique <henrique@van.huisste.de>
2018-10-22 20:32:19 +0200

    inschrijving gesloten

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -13,7 +13,7 @@ class PagesController extends Controller
 
     public function inschrijven()
     {
-        return view('inschrijven');        
+        return view('inschrijvengesloten');        
     }  
 
     public function particulier()
