ea8ce53eb0276e783098913e5f128f7132c82533
Henrique <henrique@van.huisste.de>
2019-01-27 16:06:35 +0100

    eindafrekening aangepast

--- app/Http/Controllers/BarcodeController.php
+++ app/Http/Controllers/BarcodeController.php
@@ -31,6 +31,27 @@ use App\Intertoys;
 use Custommade;
 
 
+class MyValueBinder extends PHPExcel_Cell_DefaultValueBinder implements PHPExcel_Cell_IValueBinder
+{
+    public function bindValue(PHPExcel_Cell $cell, $value = null)
+    {
+        if (is_numeric($value))
+        {
+            $cell->setValueExplicit($value, PHPExcel_Cell_DataType::TYPE_NUMERIC);
+
+            return true;
+        }
+        
+        // else return default behavior
+        return parent::bindValue($cell, $value);
+    }
+}
+
+$myValueBinder = new MyValueBinder;
+
+
+
+
 class BarcodeController extends Controller
 {
 
