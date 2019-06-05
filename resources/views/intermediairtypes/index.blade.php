936cf785a908ecf4ea728708f9ad6ae15b1e976f
henrique <henrique@van.huisste.de>
2019-06-05 19:43:40 +0200

    intermediairtypen toegevoegd

--- resources/views/intermediairtypes/index.blade.php
+++ resources/views/intermediairtypes/index.blade.php
@@ -41,11 +41,7 @@
 
 </h4>
             </div>
-            <div id="collapse{{ $faq->id }}" class="panel-collapse collapse">
-                <div class="panel-body">
-		{{ $intermediairtype->antwoord }}  
-                </div>
-            </div>
+
 	</div>
 	
 	@endforeach
@@ -56,38 +52,6 @@
 </div>
 </div>
 
-
-<style>
-    .faqHeader {
-        font-size: 27px;
-        margin: 20px;
-    }
-
-    .panel-heading [data-toggle="collapse"]:after {
-        font-family: 'Glyphicons Halflings';
-        content: "\e072"; /* "play" icon */
-        float: right;
-        color: #F58723;
-        font-size: 18px;
-        line-height: 22px;
-        /* rotate "play" icon from > (right arrow) to down arrow */
-        -webkit-transform: rotate(-90deg);
-        -moz-transform: rotate(-90deg);
-        -ms-transform: rotate(-90deg);
-        -o-transform: rotate(-90deg);
-        transform: rotate(-90deg);
-    }
-
-    .panel-heading [data-toggle="collapse"].collapsed:after {
-        /* rotate "play" icon from > (right arrow) to ^ (up arrow) */
-        -webkit-transform: rotate(90deg);
-        -moz-transform: rotate(90deg);
-        -ms-transform: rotate(90deg);
-        -o-transform: rotate(90deg);
-        transform: rotate(90deg);
-        color: #454444;
-    }
-</style>
 
 <!-- Bootstrap FAQ - END -->
 
