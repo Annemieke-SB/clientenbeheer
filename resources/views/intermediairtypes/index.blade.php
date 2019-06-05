1dda1493861bc717e379d26fcc5c50587f689a47
henrique <henrique@van.huisste.de>
2019-06-05 19:28:13 +0200

    intermediairtypen toegevoegd

--- resources/views/intermediairtypes/index.blade.php
+++ resources/views/intermediairtypes/index.blade.php
@@ -11,105 +11,39 @@
             <div class="panel panel-default">              
                 <div class="panel-body">
  
-    <h1>FAQ <small>Veelgestelde vragen</small></h1>
+    <h1>FAQ <small>Intermediairtypes</small></h1>
     <p>
                         <a href="{{ url()->previous() }}"><button type="button" class="btn btn-default navbar-btn btn-sm text-right"><span class="glyphicon glyphicon-arrow-left"></span>&nbsp;&nbsp;Terug</button></a>
                         </p>
 @if (!Auth::guest())
                         @if (Auth::user()->usertype==1)
-                        <a href="{{ url('faq/toevoegen') }}"><button type="button" class="btn btn-info navbar-btn btn-sm text-right">&nbsp;Vraag toevoegen</button></a>
+                        <a href="{{ url('intermediairtypes/toevoegen') }}"><button type="button" class="btn btn-info navbar-btn btn-sm text-right">&nbsp;Intermediairtype toevoegen</button></a>
                         @endif
 @endif
-    <!-- Bootstrap FAQ - START -->
-                                     <!-- Flashmessage -->
-                        @if (Session::get('message'))
-                        <div class="alert alert-info fade in">{{ Session::get('message')}}<a href="#" class="close" data-dismiss="alert">&times;</a></div>
-                        @endif
-	        <br />
 
-		    <div class="alert alert-warning alert-dismissible" role="alert">
-		            <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-Hier vindt u de veelgestelde vragen met betrekking tot de Cliëntenbeheer-applicatie van de Sinterklaasbank. Mocht uw vraag er niet tussenstaan, neem dan contact op met ons; <a href="mailto:info@sinterklaasbank.nl">info@sinterklaasbank.nl</a>. 
-					        </div>
 
 
     <div class="panel-group" id="accordion">
       <div class="faqHeader">Algemene vragen</div>
       
-      @foreach ($faqs as $faq)
+      @foreach ($intermediairtypes as $intermediairtype)
       @if ($faq->category == 1)
         <div class="panel panel-default">
             <div class="panel-heading">
                 <h4 class="panel-title">
-                    <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse{{ $faq->id }}">{{ $faq->vraag }}</a>
-@if (!Auth::guest())
+                    <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse{{ $intermediairtype->id }}">{{ $intermediairtype->omschrijving }}</a>
+
     @if (Auth::user()->usertype==1)
                             <a href="{{ url('faq/destroy') }}/{{$faq->id}}"><button type="button" class="btn btn-danger btn-xs text-right"><span class="glyphicon glyphicon-remove"></span>&nbsp;&nbsp;wis</button></a>
 
                             <a href="{{ url('faq/edit') }}/{{$faq->id}}"><button type="button" class="btn btn-info btn-xs text-right"><span class="glyphicon glyphicon-pencil"></span>&nbsp;&nbsp;wijzig</button></a>
     @endif
-@endif
+
 </h4>
             </div>
             <div id="collapse{{ $faq->id }}" class="panel-collapse collapse">
                 <div class="panel-body">
-		{{ $faq->antwoord }}  
-                </div>
-            </div>
-	</div>
-	@endif
-	@endforeach
-
-      <div class="faqHeader">Privacy vragen</div>
-      
-      @foreach ($faqs as $faq)
-      @if ($faq->category == 2)
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h4 class="panel-title">
-                    <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse{{ $faq->id }}">{{ $faq->vraag }}</a>
-@if (!Auth::guest())
-    @if (Auth::user()->usertype==1)
-                            <a href="{{ url('faq/destroy') }}/{{$faq->id}}"><button type="button" class="btn btn-danger btn-xs text-right"><span class="glyphicon glyphicon-remove"></span>&nbsp;&nbsp;wis</button></a>
-
-                            <a href="{{ url('faq/edit') }}/{{$faq->id}}"><button type="button" class="btn btn-info btn-xs text-right"><span class="glyphicon glyphicon-pencil"></span>&nbsp;&nbsp;wijzig</button></a>
-    @endif
-@endif
-
-		</h4>
-            </div>
-            <div id="collapse{{ $faq->id }}" class="panel-collapse collapse">
-                <div class="panel-body">
-		{{ $faq->antwoord }}  
-                </div>
-            </div>
-	</div>
-	@endif
-	@endforeach
-     
-
-      <div class="faqHeader">Gebruiker cliëntenbeheer</div>
-      
-      @foreach ($faqs as $faq)
-      @if ($faq->category == 3)
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h4 class="panel-title">
-                    <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse{{ $faq->id }}">{{ $faq->vraag }}</a>
-@if (!Auth::guest())
-    @if (Auth::user()->usertype==1)
-
-                            <a href="{{ url('faq/destroy') }}/{{$faq->id}}"><button type="button" class="btn btn-danger btn-xs text-right"><span class="glyphicon glyphicon-remove"></span>&nbsp;&nbsp;wis</button></a>
-
-                            <a href="{{ url('faq/edit') }}/{{$faq->id}}"><button type="button" class="btn btn-info btn-xs text-right"><span class="glyphicon glyphicon-pencil"></span>&nbsp;&nbsp;wijzig</button></a>
-    @endif
-@endif
-
-		</h4>
-            </div>
-            <div id="collapse{{ $faq->id }}" class="panel-collapse collapse">
-                <div class="panel-body">
-		{{ $faq->antwoord }}  
+		{{ $intermediairtype->antwoord }}  
                 </div>
             </div>
 	</div>
