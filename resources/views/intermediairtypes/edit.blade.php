0dd930fcc2a417fe9ab9b6d74c1c86c1b777b2fa
henrique <henrique@van.huisste.de>
2019-06-08 13:44:47 +0200

    intermediairtype edit toegevoegd

--- resources/views/intermediairtypes/edit.blade.php
+++ resources/views/intermediairtypes/edit.blade.php
@@ -11,46 +11,36 @@
 
                 <ol class="breadcrumb">
                   <li><a href="{{ url('/home') }}">Home</a></li>
-                  <li class="active">Faq wijzigen</li>
+                  <li><a href="{{ url('/blacklist') }}">Intermediairtypes</a></li>
+                  <li class="active">Item wijzigen</li>
                 </ol> 
 
                 <div class="panel-body">
                         <p>
-                        <a href="{{ url('faq') }}"><button type="button" class="btn btn-default navbar-btn btn-sm text-right"><span class="glyphicon glyphicon-arrow-left"></span>&nbsp;&nbsp;Terug</button></a>
+                        <a href="{{ url('intermediairtypes') }}"><button type="button" class="btn btn-default navbar-btn btn-sm text-right"><span class="glyphicon glyphicon-arrow-left"></span>&nbsp;&nbsp;Terug</button></a>
                         </p>
 
 
             <!-- Flashmessage -->
             @if (Session::get('message'))
             <div class="alert alert-info fade in">{{ Session::get('message')}}<a href="#" class="close" data-dismiss="alert">&times;</a></div>
-	    @endif      
+        @endif      
 
 
 
 
-                    {!! Form::open(['url' => 'faq/update', 'id' => 'createform']) !!}
+                    {!! Form::open(['url' => 'intermediairtypes/update', 'id' => 'createform']) !!}
                         
                         <div class="form-group">
 
-                            {!! Form::label('vraag', 'Vraag') !!}
-                            {!! Form::text('vraag', $faq->vraag, ['class' => 'form-control','required', 'autofocus']) !!}
+                            {!! Form::label('omschrijving', 'Omschrijving') !!}
+                            {!! Form::text('omschrijving', $intermediairtype->omschrijving, ['class' => 'form-control','required', 'autofocus']) !!}
 
                         </div>
-                        <div class="form-group">
-
-                            {!! Form::label('antwoord', 'Antwoord') !!}
-                            {!! Form::textarea('antwoord', $faq->antwoord, ['class' => 'form-control', 'required','autofocus']) !!}
-
-                        </div>
-                        <div class="form-group">
-
-                            {!! Form::label('category', 'Categorie') !!}
-                            {!! Form::select('category', [''=>'-', 1 =>'Algemeen', 2 =>'Privacy', 3 => 'Gebruik'], $faq->category,  ["class"=>"form-control", 'autofocus required']) !!}
-
-			</div>
-         		<div class="form-group">
+                      
+                <div class="form-group">
                             {!! Form::hidden('user_id', $user_id) !!}
-                            {!! Form::hidden('id', $faq->id) !!}
+                            {!! Form::hidden('id', $intermediairtype->id) !!}
 
                             <input class="btn btn-primary form-control" type="submit" id="verzendknop" value="Opslaan">
                             
@@ -67,30 +57,4 @@
 </div>
 
 
-<script type="text/javascript">
-
-// Dit script zorgt voor sorteren in de lijst
-$('th').click(function(){
-    var table = $(this).parents('table').eq(0)
-    var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()))
-    this.asc = !this.asc
-    if (!this.asc){rows = rows.reverse()}
-    for (var i = 0; i < rows.length; i++){table.append(rows[i])}
-})
-function comparer(index) {
-    return function(a, b) {
-        var valA = getCellValue(a, index), valB = getCellValue(b, index)
-        return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.localeCompare(valB)
-    }
-}
-function getCellValue(row, index){ return $(row).children('td').eq(index).html() }
-
-$(document).ready(function() {
-// Hier om de tooltips te activeren
-$('[data-toggle="tooltip"]').tooltip();
-});
-
-
-
-</script>
 @endsection
