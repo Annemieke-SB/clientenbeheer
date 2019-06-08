8289b75217ad02dfbbca594083ca1ea1dee24e10
henrique <henrique@van.huisste.de>
2019-06-08 14:43:42 +0200

    intermediairtype edit toegevoegd

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">Inschrijven</div>
                 <div class="panel-body">
 
-<?php use App\Http\Controllers\Intermediairtype;
+<?php use App\Http\Controllers\IntermediairtypeController;
 echo Intermediairtype::functionName(); ?>
 
 
