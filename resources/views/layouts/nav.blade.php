45c0e2c18725a699515212c134d0363e205c2d75
henrique <henrique@van.huisste.de>
2019-06-10 08:19:40 +0200

    menuitems te keuren gezinnen toegevoegd

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -45,11 +45,11 @@
       <ul class="dropdown-menu">
         <li><a href="{{ url('/users/index') }}"><span class="glyphicon glyphicon-user" aria-hidden="true"></span>&nbsp;Gebruikersbeheer</a></li>
         <li role="separator" class="divider"></li>
-        <li><a href="{{ url('/users/index?filter=igg') }}"><span class="glyphicon glyphicon-user" aria-hidden="true"></span>&nbsp;Keuren gezinnen</a></li>        
-        <li><a href="{{ url('/tellingen/') }}">Tellingen</a></li>
-        <li><a href="{{ url('/maillijsten/') }}">Maillijsten</a></li>
+        <li><a href="{{ url('/users/index?filter=igg') }}"><span class="glyphicon glyphicon-unchecked" aria-hidden="true"></span>&nbsp;Keuren gezinnen</a></li>        
+        <li><a href="{{ url('/tellingen/') }}"><span class="glyphicon glyphicon-equalizer" aria-hidden="true"></span>&nbsp;Tellingen</a></li>
+        <li><a href="{{ url('/maillijsten/') }}"><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span>&nbsp;Maillijsten</a></li>
         <li role="separator" class="divider"></li>
-        <li><a href="{{ url('/barcodes/') }}">Tegoedcodes</a></li>
+        <li><a href="{{ url('/barcodes/') }}"><span class="glyphicon glyphicon-barcode" aria-hidden="true"></span>&nbsp;Barcodes</a></li>
       </ul>
     </li>
     <li class="dropdown">
