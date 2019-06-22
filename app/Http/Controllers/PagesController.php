d9dcf2abc4eecf40b612f0cc1ea94a7dd09e95a2
henrique <henrique@van.huisste.de>
2019-06-22 09:22:17 +0200

    inschrijvingen intermediair niet mogelijk maken na sluiten

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use DB;
-use App\Setting;
+use Settings;
 
 
 
@@ -16,7 +16,7 @@ class PagesController extends Controller
     public function inschrijven()
     {
         dd(Setting::getReadable());
-        
+
         if ($settings["inschrijven_gesloten"] == 1) {
             return view('inschrijvengesloten'); 
         } else {
