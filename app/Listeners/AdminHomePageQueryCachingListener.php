de6a7a0c33a1c18d8d70c26dc03c1f6f0109b358
henrique <henrique@van.huisste.de>
2019-06-23 20:43:40 +0200

    events en listeners aangemaakt voor de adminpage

--- app/Listeners/AdminHomePageQueryCachingListener.php
+++ app/Listeners/AdminHomePageQueryCachingListener.php
@@ -34,23 +34,23 @@ class AdminHomePageQueryCachingListener
 
         // Oude cache is al in de HomeController geleegd. 
 
-                    dd('hier');
+                    
 
         // Nieuwe cache aanleggen
 
-                    $intermediairzonderfamilies = Cache::rememberForever('intermediairzonderfamilies', function () {
+                    Cache::rememberForever('intermediairzonderfamilies', function () {
                         return User::where('usertype',3)->whereDoesntHave('familys')->where('activated', 1)->get();
                     });
-                    $familieszonderkinderen = Cache::rememberForever('familieszonderkinderen', function () {
+                    Cache::rememberForever('familieszonderkinderen', function () {
                         return Family::whereDoesntHave('kids')->get();
                     });
-                    $nogtekeuren_families = Cache::rememberForever('nogtekeuren_families', function () {
+                    Cache::rememberForever('nogtekeuren_families', function () {
                         return Family::where([['aangemeld', 1],['goedgekeurd', 0]])->get();
                     });
-                    $nogtekeuren_users = Cache::rememberForever('nogtekeuren_users', function () {
+                    Cache::rememberForever('nogtekeuren_users', function () {
                         return User::where([['activated', 0],['emailverified', 1]])->get();
                     });
-                    $intermediairmetnietgedownloadepdfs = Cache::rememberForever('intermediairmetnietgedownloadepdfs', function () {
+                    Cache::rememberForever('intermediairmetnietgedownloadepdfs', function () {
                         return User::whereHas('barcodes', function($query){
                             $query->whereNull('downloadedpdf');
                         })->get();
