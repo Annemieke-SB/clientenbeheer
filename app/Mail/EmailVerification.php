a2102d488d8d358f7081fea6374ee3ed70782ee8
henrique <henrique@van.huisste.de>
2019-07-05 19:11:21 +0200

    sorteren intermediairtypen

--- app/Mail/EmailVerification.php
+++ app/Mail/EmailVerification.php
@@ -32,7 +32,7 @@ class EmailVerification extends Mailable
     public function build()
     {
         return $this->from('noreply@sinterklaasbank.nl')
-                    ->subject('Bericht van de Sinterklaasbank: verifiëer uw emailadres')
+                    ->subject('Bericht van de Sinterklaasbank: bevestig uw emailadres')
                     ->view('emails.verification');
     }
 }
