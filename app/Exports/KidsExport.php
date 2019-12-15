c412d76715d408543d6aa87a8dd42708498ba802
henrique <henrique@pcboven.Home>
2019-12-15 12:38:14 +0100

    exportfunctie werkt

--- app/Exports/KidsExport.php
+++ app/Exports/KidsExport.php
@@ -19,6 +19,7 @@ class KidsExport implements FromCollection, WithHeadings
             ->join('users', 'users.id', '=', 'kids.user_id')
             ->join('familys', 'familys.id', '=', 'kids.family_id')
             ->select('kids.voornaam', 'familys.achternaam', 'users.email', 'users.organisatienaam')
+            ->where('kids.goedgekeurd', '=', 1)
             ->get();
 
 
